4c982f2cef75d5ae643dc1edc3e17125861074e4
Robert-Mahanna <robertcmahannajr@gmail.com>
2024-11-30 20:05:18 -0600

    Added in shooting and asteroid splitting.

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
+import sys
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
-    
     game_on = True
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
     clock = pygame.time.Clock()
-    dt = 0
-
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    asteroid_field = AsteroidField()
+
     Player.containers = (updatable, drawable)
-    
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
+ 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
     
     while game_on:
         for event in pygame.event.get():
@@ -28,7 +36,16 @@ def main():
         for thing in updatable:
             thing.update(dt)
 
-        screen.fill((0, 0, 0))
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    asteroid.split()
+                    shot.kill()
+
+        screen.fill("black")
         
         for thing in drawable:
             thing.draw(screen)
